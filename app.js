b7f007c7e668176531ef618c166c6960c446079f
Arthur Oliveira <arthuroliveira@MacBook-Pro-de-Arthur.local>
2018-04-08 23:46:14 -0300

    Commit inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+const APIKEY = '4c48e10ab72d4baa8feff20c49cac921'
+// Querying main tag
+
+
+window.addEventListener('load', data => {
+    updateNews()
+})
+
+async function updateNews(){
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${APIKEY}`)
+    const json = await res.json()
+    const main = document.querySelector("main")
+    
+    main.innerHTML = json.articles.map(createArticle).join('\n')
+}
+
+function createArticle(article){
+
+    return `
+        <div class= 'article'> 
+            <a href = "${article.url}">
+                <h2>${article.title}</h2>
+                <image src = "${article.urlToImage}">
+                <p>"${article.description}"</p>
+            </a>
+        </div>
+    `
+}
